e2492d9148f0998181dabfec7c89b5ec7fe55058
Rashid <rashidgaitov@gmail.com>
2023-09-29 16:23:59 +0300

    Added logos

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import logoDark from "./partly-sunny-outline.svg"
+import logoLight from "./partly-sunny-outline.svg"
+import cartImg from "./cart-outline.svg"
+import paymentLogo from "./card-outline.svg"
+import googleLogo from "./logo-google.svg"
+import githubLogo from "./logo-github.svg"
+
+export {logoDark, logoLight, cartImg, paymentLogo, googleLogo, githubLogo}
