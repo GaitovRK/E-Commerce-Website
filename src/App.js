26c2200aba9c12ba2214d63c30eb07c1c2710958
Rashid <rashidgaitov@gmail.com>
2023-09-29 18:14:58 +0300

    Added header elements and styled them

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <div className=" font-bodyFont">
       <Header />
 
       <Home />      
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import {logoDark} from '../assets/index'
 
 const Header = () => {
   return (
-    <div className='w-full h-20 bg-white border-b-[1px] border-b-gray-800'>Header</div>
+    <div className='w-full h-20 bg-white border-b-[1px] border-b-gray-800'>
+      
+      <div className=' max-w-screen-xl h-full mx-auto flex items-center justify-between'>
+        <div>
+          <img className='h-16' src={logoDark} alt=''/>
+        </div>
+
+        <div>
+          <ul className='flex gap-8 items-center'>
+            <li className='text-base text-black font-titleFont hover:text-orange-900 hover:underline underline-offset-2 hover:underline decoration-[1px] cursor-pointer duration-300'>Home</li>
+            <li className='text-base text-black font-titleFont hover:text-orange-900 hover:underline underline-offset-2 hover:underline decoration-[1px] cursor-pointer duration-300'>Pages</li>
+            <li className='text-base text-black font-titleFont hover:text-orange-900 hover:underline underline-offset-2 hover:underline decoration-[1px] cursor-pointer duration-300' >Shop</li>
+            <li className='text-base text-black font-titleFont hover:text-orange-900 hover:underline underline-offset-2 hover:underline decoration-[1px] cursor-pointer duration-300' >Element</li>
+            <li className='text-base text-black font-titleFont hover:text-orange-900 hover:underline underline-offset-2 hover:underline decoration-[1px] cursor-pointer duration-300' >Blog</li>
+          </ul>
+        </div>
+
+
+      </div>
+    </div>
   )
 }
 
